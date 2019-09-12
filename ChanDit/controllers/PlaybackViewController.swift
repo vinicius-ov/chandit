4fe767602d8ea0f1ff7691a0dfe332612dfabb86
Vinicius Valvassori <vinicius.ov@gmail.com>
2019-09-12 17:13:55 -0300

    better navigation for webm

--- ChanDit/controllers/PlaybackViewController.swift
+++ ChanDit/controllers/PlaybackViewController.swift
@@ -30,6 +30,7 @@ class PlaybackViewController: UIViewController {
         mediaPlayer.delegate = self
         mediaPlayer.drawable = movieView
         mediaPlayer.media = VLCMedia(url: url)
+        mediaPlayer.audio.volume = 0
     }
     
     @IBAction func handlePlayPause(_ sender: UIButton) {
@@ -41,14 +42,27 @@ class PlaybackViewController: UIViewController {
             sender.isSelected = false
         }
     }
+    
+    @IBAction func handleToggleAudio(_ sender: UIButton) {
+        if sender.isSelected {
+            mediaPlayer.audio.volume = 0
+            sender.isSelected = false
+        } else {
+            mediaPlayer.audio.volume = 100
+            sender.isSelected = true
+        }
+    }
+    
+    @IBAction func closeView(_ sender: Any) {
+        mediaPlayer.stop()
+        self.dismiss(animated: true, completion: nil)
+    }
 }
 
 extension PlaybackViewController: VLCMediaPlayerDelegate {
-    
     func mediaPlayerStateChanged(_ aNotification: Notification!) {
         if mediaPlayer.state == .stopped {
             self.dismiss(animated: true, completion: nil)
         }
     }
 }
-
