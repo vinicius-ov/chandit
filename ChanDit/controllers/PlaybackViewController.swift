3991572b1735d4a83b4eabb73142ba3be7c4a0b6
Vinicius <vinicius.ov@gmail.com>
2019-10-23 13:32:57 -0300

    add save button export failing

--- ChanDit/controllers/PlaybackViewController.swift
+++ ChanDit/controllers/PlaybackViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AVKit
 
 class PlaybackViewController: UIViewController {
     //let mediaURL:String? = nil
@@ -64,6 +65,17 @@ class PlaybackViewController: UIViewController {
         guard let url = mediaURL else { return }
         mediaPlayer.media = VLCMedia(url: url)
     }
+    
+    @IBAction func saveVideo(_ sender: Any) {
+        guard let url = mediaURL else { return }
+        encodeVideo(at:
+//            url
+        URL(string: "https://file-examples.com/wp-content/uploads/2018/04/file_example_MOV_480_700kB.mov")!
+        ) { (exportUrl, error) in
+            print(exportUrl)
+            print(error)
+        }
+    }
 }
 
 extension PlaybackViewController: VLCMediaPlayerDelegate {
@@ -76,5 +88,63 @@ extension PlaybackViewController: VLCMediaPlayerDelegate {
     func mediaPlayerTimeChanged(_ aNotification: Notification!) {
         print(mediaPlayer.time.debugDescription)
         print(mediaPlayer.remainingTime.debugDescription)
+    }
+}
+
+extension PlaybackViewController {
+    func encodeVideo(at videoURL: URL, completionHandler: ((URL?, Error?) -> Void)?)  {
+        let avAsset = AVURLAsset(url: videoURL, options: nil)
+            
+        let startDate = Date()
+            
+        print(AVAssetExportSession.exportPresets(compatibleWith: avAsset))
+        //Create Export session
+        guard let exportSession = AVAssetExportSession(asset: avAsset, presetName: AVAssetExportPresetLowQuality) else {
+            completionHandler?(nil, nil)
+            return
+        }
+            
+        //Creating temp path to save the converted video
+        let documentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0] as URL
+        let filePath = documentsDirectory.appendingPathComponent("rendered-Video.mp4")
+            
+        //Check if the file already exists then remove the previous file
+        if FileManager.default.fileExists(atPath: filePath.path) {
+            do {
+                try FileManager.default.removeItem(at: filePath)
+            } catch {
+                completionHandler?(nil, error)
+            }
+        }
+            
+        exportSession.outputURL = filePath
+        exportSession.outputFileType = AVFileType.mp4
+        exportSession.shouldOptimizeForNetworkUse = false
+        let start = CMTimeMakeWithSeconds(0.0, preferredTimescale: 0)
+        let range = CMTimeRangeMake(start: start, duration: avAsset.duration)
+        exportSession.timeRange = range
+            
+        exportSession.exportAsynchronously(completionHandler: {() -> Void in
+            switch exportSession.status {
+            case .failed:
+                print(exportSession.error ?? "NO ERROR")
+                completionHandler?(nil, exportSession.error)
+            case .cancelled:
+                print("Export canceled")
+                completionHandler?(nil, nil)
+            case .completed:
+                //Video conversion finished
+                let endDate = Date()
+                    
+                let time = endDate.timeIntervalSince(startDate)
+                print(time)
+                print("Successful!")
+                print(exportSession.outputURL ?? "NO OUTPUT URL")
+                completionHandler?(exportSession.outputURL, nil)
+                    
+                default: break
+            }
+                
+        })
     }
 }
